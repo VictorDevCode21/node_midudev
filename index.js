5211efbc9258275ad2ac248af1e75034f4b54860
Victor <victorenrique2002@gmail.com>
2025-03-01 12:05:14 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,6 @@
+
+// Common JS require module
+const {sum }= require('./sum')
+
+
+console.log(sum(1,2))
---
+++ sum.js
@@ -0,0 +1,11 @@
+function sum (a, b) {
+    return a + b
+}
+
+
+
+// Common JS Module Export
+module.exports = {
+    sum
+}
+
