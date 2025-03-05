24fc13b473b4ebf22693b26e24d0228130c2c7ae
Victor <victorenrique2002@gmail.com>
2025-03-05 04:05:11 -0400

    ls simulation in node created

--- 6.ls.js
+++ 6.ls.js
@@ -1,5 +1,38 @@
 
+// eslint-disable-next-line no-undef
+const fs = require("node:fs/promises")
 
+
+// simulate ls command in nodejs using callbacks
+// fs.readdir('.', (e, files) => {
+//     if (e) {
+//         console.log("Error reading directory: " + e)
+//         return;
+//     }
+
+
+//     files.forEach(file => {
+//         console.log(file);
+//     })
+
+// })
+
+console.log(fs.stat())
+
+
+// simulate ls command in nodejs using promises
+fs.readdir('.')
+    .then(files => {
+        files.forEach(file => {
+            console.log(file);
+        })
+    })
+    .catch(e => {
+        console.log("Error reading directory: " + e)
+        return;
+    })
+
+    // 
 
 
 
